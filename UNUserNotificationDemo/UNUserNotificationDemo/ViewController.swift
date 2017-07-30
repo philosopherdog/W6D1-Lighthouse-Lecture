9998877ab65090d09825dfe8b77da8f6604f63b4
steve thompson <steven.thompson.ca@gmail.com>
2017-07-30 15:46:02 -0400

    adds UNUserNotification project

---
+++ UNUserNotificationDemo/UNUserNotificationDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UNUserNotificationDemo
+//
+//  Created by steve on 2017-07-30.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+import UIKit
+
+// required for new notifications framework
+import UserNotifications
+
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func buttonTapped() {
+  
+  }
+}
